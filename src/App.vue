8a4027397d5a3d217085020bf0b3c3151828faee
thang <ntthang4@cmcglobal.vn>
2026-04-22 14:02:45 +0700

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import {onMounted} from "vue";
+import gatewayUrl from "@/api/authApi.ts";
+import {connectWebSocket} from "@/services/ws.ts";
+
+onMounted(async () => {
+  const res = await gatewayUrl.get('/api/auth/checkLogin')
+
+  if (res.data.isLoggedIn) {
+    connectWebSocket()
+  }
+})
 </script>
 
 <template>
