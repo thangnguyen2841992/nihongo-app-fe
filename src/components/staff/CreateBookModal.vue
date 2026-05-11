ef019ba6307ddd0e8c066891966f9215af5625e3
thang <ntthang4@cmcglobal.vn>
2026-05-11 15:58:09 +0700

    ...

--- src/components/staff/CreateBookModal.vue
+++ src/components/staff/CreateBookModal.vue
@@ -2,7 +2,14 @@
 import {onMounted, ref} from "vue"
 import axios from "axios"
 import gatewayUrl from "@/api/authApi.ts";
+import {
+  ref as storageRef,
+  uploadBytes,
+  getDownloadURL
+} from "firebase/storage"
+import {storage} from "@/services/firebase.ts";
 
+const files = ref<File[]>([])
 const emit = defineEmits(["close", "created"])
 interface Type {
   typeId: number
@@ -59,11 +66,12 @@ onMounted(async () => {
 const submit = async () => {
   loading.value = true
   try {
+    const urls = await uploadFiles()
     await axios.post("http://localhost:8080/api/staff/books", {
       bookName: form.value.bookName,
       typeId: form.value.typeId,
       levelId: form.value.levelId,
-      urls: form.value.urls.split(",")
+      urls: urls
     })
 
     emit("created")
@@ -71,6 +79,43 @@ const submit = async () => {
     loading.value = false
   }
 }
+
+const handleFiles = (
+  e: Event
+) => {
+
+  const target =
+    e.target as HTMLInputElement
+
+  if (!target.files) return
+
+  files.value =
+    Array.from(target.files)
+}
+
+const uploadFiles = async () => {
+
+  const urls: string[] = []
+
+  for (const file of files.value) {
+
+    const fileRef = storageRef(
+      storage,
+      `books/${Date.now()}_${file.name}`
+    )
+
+    await uploadBytes(fileRef, file)
+
+    const url =
+      await getDownloadURL(fileRef)
+
+    urls.push(url)
+  }
+
+  return urls
+}
+
+
 </script>
 
 <template>
@@ -118,14 +163,19 @@ const submit = async () => {
           </div>
 
           <div class="mb-3">
-            <label class="form-label">URLs</label>
-            <input
-              v-model="form.urls"
-              class="form-control"
-              placeholder="url1,url2"
-            />
-          </div>
 
+            <label class="form-label">
+              Upload Files
+            </label>
+
+            <input
+              type="file"
+              multiple
+              class="form-control"
+              @change="handleFiles"
+            />
+
+          </div>
         </div>
 
         <!-- FOOTER -->
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,15 +1,16 @@
-import { initializeApp } from "firebase/app"
+import {initializeApp} from "firebase/app"
 import {
   getStorage
 } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "XXX",
-  appId: "XXX"
+  apiKey: "AIzaSyAO2Rcfya7k19xVUrKkVVn5b8igAXgUy5o",
+  authDomain: "nihongo-app-555f3.firebaseapp.com",
+  projectId: "nihongo-app-555f3",
+  storageBucket: "nihongo-app-555f3.appspot.com",
+  messagingSenderId: "444924348433",
+  appId: "1:444924348433:web:9acc40edefe114f3d189b0",
+  measurementId: "G-TV044DD2H7"
 }
 
 const app =
